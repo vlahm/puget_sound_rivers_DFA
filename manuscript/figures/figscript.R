c6a1796755b73ae27d7a1e0dba4788e72bbffe03
mike <vlahm@uw.edu>
2017-01-18 16:31:06 -0800

    completed figures 1-4 (1 being the map)

--- manuscript/figures/figscript.R
+++ manuscript/figures/figscript.R
@@ -8,7 +8,7 @@ rm(list=ls()); cat('\014') #clear env and console
 # 0 - setup ####
 
 setwd('C:/Users/Mike/git/stream_nuts_DFA/manuscript/figures')
-setwd('~/git/puget_sound_rivers_DFA/manuscript/figures')
+# setwd('~/git/puget_sound_rivers_DFA/manuscript/figures')
 
 library(viridis)
 library(plotrix)
@@ -43,17 +43,20 @@ land_sub <- land[,landcols] #subset landscape variables by those used in the ana
 
 #% of watershed area classified as ice/snow land cover (NLCD 2011 class 12)
 #% of watershed area classified as ice/snow land cover (NLCD 2006 class 12)
-pdf('effect_size_reg.pdf', width=7, height=6)
+pdf('01_effect_size_reg.pdf', width=7, height=6)
 defpar <- par(mar=c(5,5,2,5))
 pal <- colorRampPalette(c('brown', 'white'))
 cols <- pal(10)[as.numeric(cut(land$ElevWs, breaks=10))]
-plot(land$Ice06_11, rescaled_effect_size,
-     xlab='Watershed % ice cover', main='', bg=cols, col='black',
+plot(land$Ice06_11, rescaled_effect_size, type='n',
+     xlab='Watershed % ice cover', main='',
      ylab=expression(paste(Delta, ' stream', degree, 'C ',
-                            Delta, ' air', degree, 'C'^-1)),
-     pch=21, cex=1.5, cex.lab=1.3, cex.axis=1, font=2, lwd=2)
+                           Delta, ' air', degree, 'C'^-1)),
+     cex.lab=1.3, cex.axis=1, font=2)
 mod <- lm(rescaled_effect_size ~ land$Ice06_11)
 abline(mod, col='gray', lty=2, lwd=3)
+points(land$Ice06_11, rescaled_effect_size,
+     bg=cols, col='black',
+     pch=21, cex=1.5, lwd=2)
 color.legend(xl=4,xr=4.4,yb=0.5, yt=0.6, legend=c('147', '1349'),
              rect.col=colorRampPalette(c('brown', 'white'))(10),
              align='r', gradient='y')
@@ -64,16 +67,17 @@ dev.off()
 
 # 2 - effect size regression ####
 
-pdf('loadings_reg.pdf', width=7, height=6)
+pdf('02_loadings_reg.pdf', width=7, height=6)
 defpar <- par(mar=c(5,5,2,5))
 pal <- colorRampPalette(c('brown', 'white'))
 cols <- pal(10)[as.numeric(cut(land$ElevWs, breaks=10))]
 plot(land$Ice06_11, dfa$Estimates$Z[,1],
-     xlab='Watershed % ice cover', ylab='Trend 1 loadings',
-     main='', col='black', bg=cols,
-     pch=21, cex=1.5, cex.lab=1.3, cex.axis=1, font=2, lwd=2)
-mod <- lm(dfa$Estimates$Z[,1] ~ land$Ice06_11)
+     xlab='Watershed % ice cover', ylab='Trend 1 loadings', type='n',
+     main='', cex.lab=1.3, cex.axis=1, font=2)
 abline(mod, col='gray', lty=2, lwd=3)
+points(land$Ice06_11, dfa$Estimates$Z[,1], col='black', bg=cols,
+       pch=21, cex=1.5, lwd=2)
+mod <- lm(dfa$Estimates$Z[,1] ~ land$Ice06_11)
 color.legend(xl=4,xr=4.4,yb=1.32, yt=1.82, legend=c('147', '1349'),
              rect.col=colorRampPalette(c('brown', 'white'))(10),
              align='r', gradient='y')
@@ -84,25 +88,31 @@ dev.off()
 
 # 3 - effect size by month ####
 
-pdf('loadings_reg.pdf', width=7, height=6)
+pdf('03_eff_size_bymonth.pdf', width=8, height=8)
 rescaled_seas <- apply(dfa$Estimates$D[,1:12], 2, function(x) x * trans$sds)
-defpar <- par(mfrow=c(3,2))
+defpar <- par(mfrow=c(4,3), oma=c(5,5,1,1), mar=c(0.5,0.5,0.5,0.5))
 pal <- colorRampPalette(c('brown', 'white'))
 cols <- pal(10)[as.numeric(cut(land$BFIWs, breaks=10))]
 for(i in 1:12){
     mod <- lm(rescaled_seas[,i] ~ land$Ice06_11)
     slope <- round(unname(mod$coefficients[2]), 2)
-    plot(land$Ice06_11, rescaled_seas[,i], main='',
-         ylab=paste(month.abb[i]), xlab='',
-         ylim=c(min(rescaled_seas), max(rescaled_seas)), col='black',
-         pch=21, cex=1.5, cex.lab=1.3, cex.axis=1, font=2, bg=cols)
-    abline(mod, col='gray', lty=2, lwd=2.5)
-    abline(h=0, col='blue', lwd=1, lty=1)
+    plot(land$Ice06_11, rescaled_seas[,i], main='', yaxt='n', xaxt='n',
+         ylab=paste(month.abb[i]), xlab='', type='n', bty='l',
+         ylim=c(min(rescaled_seas), max(rescaled_seas)))
+    abline(h=0, col='royalblue', lwd=2, lty=1)
+    abline(mod, col='gray40', lty=2, lwd=2.5)
+    points(land$Ice06_11, rescaled_seas[,i], col='black', pch=21,
+           cex=1.5, cex.lab=1.3, cex.axis=1, font=2, bg=cols)
     sig <- ifelse(summary(mod)$coefficients[2,4]<=0.1, '*', '')
-    print.letter(label=paste0('m = ', slope, sig), xy=c(0.72,0.9), cex=1.2, font=1, col="black", pos=4)
+    print.letter(label=substitute(paste(x, '. ', italic('m'), ' = ', y, z),
+                                  list(x=month.abb[i], y=sprintf('%+1.2f', slope), z=sig)),
+                 xy=c(0.5,0.9), cex=1.2, font=1, col="black", pos=4)
+    if(i %in% 10:12) axis(1)
+    if(i %in% c(1,4,7,10)) axis(2, las=2)
 }
+mtext('Watershed % ice cover', side=1, outer=TRUE, line=3, font=2)
+mtext(expression(paste(bold('Monthly')~bold(Delta)~bold('stream')~bold(degree)~bold('C ')~
+                     bold(Delta)~bold('air')~bold(degree)~bold('C')^-1, sep='')),
+      side=2, outer=TRUE, line=3)
 par(defpar)
 dev.off()
-
-, 'change in water temp' (really the effect size though, right?
-                          make m italic?
